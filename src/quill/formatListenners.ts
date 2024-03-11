6b1e204b5259460401e22b9f020fd782a7db7b96
jaenfigueroa <jaenfigueroa03042000@gmail.com>
2024-03-11 09:45:34 -0500

    obtener formatos en comun de un texto seleccionado

--- src/quill/formatListenners.ts
+++ src/quill/formatListenners.ts
@@ -1,6 +1,34 @@
 import quill from '.'
 
+/* 
+  ESTILOS/FORMATOS APLICABLES
+  https://quilljs.com/docs/formats
+*/
+
+// Boton para cambiar el formato del texto/lineas seleccionado // SOLO SE PUEDE APLICAR FORMATOS -> INLINE
 document.querySelector<HTMLButtonElement>('#formatButton')!.onclick = () => {
   quill.format('color', 'red')
-  quill.format('align', 'right')
+  quill.format('background', 'yellow')
+  quill.format('size', 'huge') // small, large, huge, default: normal
 }
+
+// Boton para cambiar el formato de las lineas de un rango dado // SOLO SE PUEDE APLICAR FORMATOS -> BLOCK
+document.querySelector<HTMLButtonElement>('#formatLIneButton')!.onclick = () => {
+  quill.formatLine(0, 100, {
+    'align': 'right',
+    'list': 'ordered'
+  })
+}
+
+// Boton para cambiar el formatear el texto en un rango dado
+document.querySelector<HTMLButtonElement>('#formatTextButton')!.onclick = () => {
+  quill.formatText({ index: 0, length: 10 }, 'bold', true); // <-- // confictivo - revisar luego
+}
+
+// Boton para obtener los formatos en comun del (texto en un rango)/seleccionado o del cursor actual
+document.querySelector<HTMLButtonElement>('#getFormatButton')!.onclick = () => {
+  console.log(quill.getFormat()) // <-- del cursor actual/texto seleccionado
+  // console.log(quill.getFormat(0, 10)) <-- en un rango dado
+}
+
+
