27b167f6741f6df311fdef38a33a2b12f66ba7d3
jaenfigueroa <jaenfigueroa03042000@gmail.com>
2024-03-12 10:50:49 -0500

    iniciar extension

---
+++ src/quill/model.ts
@@ -0,0 +1,16 @@
+// const container = document.querySelector('#container');
+
+// console.log(container)
+
+// const quill = new Quill(container);
+// console.log(Quill.find(container) === quill); // Should be true
+
+// quill.insertText(0, 'Hello', 'link', 'https://world.com');
+
+// const linkNode = document.querySelector('#container a');
+// const linkBlot = Quill.find(linkNode);
+
+// // Find Quill instance from a blot
+// console.log(Quill.find(linkBlot.scroll.domNode.parentElement));
+
+// ??????????????????'
